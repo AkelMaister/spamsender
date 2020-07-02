ac45024f09cde68e9049656126cec8d2cd2bb6c7
root <root@DESKTOP-78C6K6A.localdomain>
2020-07-02 12:45:39 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+require('dotenv').config()
+
+const nodemailer = require("nodemailer");
+const readline = require('readline');
+const fs = require('fs');
+
+const readFile = (filename, encoding) => {
+  try {
+    return fs.readFileSync(filename).toString(encoding);
+  }
+  catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+const textBody=readFile("mail.txt","utf8")
+const htmlBody=readFile("mail.html","utf8")
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('maillist.txt'),
+    output: process.stdout,
+    console: false
+});
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: parseInt(process.env.MAIL_PORT),
+  secure: (process.env.MAIL_SECURE === 'true'), // true for 465, false for other ports
+  auth: {
+    user: process.env.MAIL_USER, // generated ethereal user
+    pass: process.env.MAIL_PASSWORD, // generated ethereal password
+  },
+});
+
+readInterface.on('line', function(line) {
+  sendMail(line).catch((err) => {
+    console.log(err)
+    fs.appendFile('error.txt', line + "\n", function (err) {
+      if (err) throw err;
+      console.log(line + " " + err)
+    });
+  });
+})
+
+async function sendMail(mailTo) {
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Gamzix" <sales@gamzix.com>', // sender address
+    to: mailTo, // list of receivers
+    subject: "Go Wild test mail", // Subject line
+    text: textBody, // plain text body
+    html: htmlBody, // html body
+  })
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  fs.appendFile('success.txt', mailTo + "\n", function (err) {
+    if (err) throw err;
+    console.log("SUCCESS: " + mailTo + " - " + info.messageId + "\n");
+  });
+}
